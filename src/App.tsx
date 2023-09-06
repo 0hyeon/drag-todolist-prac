513be826f5a0da13a68fefe7b16e038f17481bed
0hyeon <djdjdjk2006@naver.com>
2023-09-06 14:46:11 +0900

    feat: components create

---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import styled from "styled-components";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 400px;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Boards = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(1, 1fr);
+`;
+const Board = styled.div`
+  padding: 2px 10px;
+  padding-top: 30px;
+  background-color: ${(props) => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 200px;
+  background-color: #ddd;
+`;
+const Card = styled.div`
+  border-radius: 5px;
+  padding: 10px 10px;
+  background-color: #fff;
+  margin-bottom: 5px;
+`;
+const toDos = ["a", "b", "c", "d", "e", "f", "g", "h"];
+function App() {
+  const onDragEnd = () => {};
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Wrapper>
+        <Boards>
+          <Droppable droppableId="one">
+            {(magic) => (
+              <Board ref={magic.innerRef} {...magic.droppableProps}>
+                {toDos.map((toDo, index) => (
+                  <Draggable draggableId={toDo} index={index}>
+                    {(magic) => (
+                      <Card
+                        ref={magic.innerRef}
+                        {...magic.dragHandleProps}
+                        {...magic.draggableProps}
+                      >
+                        {toDo}
+                      </Card>
+                    )}
+                  </Draggable>
+                ))}
+                {/* 사이즈 안변하고 유지 */}
+                {magic.placeholder}
+              </Board>
+            )}
+          </Droppable>
+        </Boards>
+      </Wrapper>
+    </DragDropContext>
+  );
+}
+
+export default App;
